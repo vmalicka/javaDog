828b2759bbd5daa8e70b2a3009ac926ecddcc701
ValerieMalicka <vqm5328@psu.edu>
2023-08-30 12:33:34 -0400

    Dog project

--- Dog.java
+++ Dog.java
@@ -1,4 +1,26 @@
 public class Dog {
 
+    // Properties
+    private String name;
+    private int age;
+    private String color;
+    private int weight;
+    private String breed;
+
+    //constructor
+    public Dog(String name, int age, String color, int weight, String breed){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.weight = weight;
+        this. breed = breed;
+    }
+
+    // methods
+    public void bark(){
+        System.out.println(name + " says: bark!");
+    }
+
+
 
 }
