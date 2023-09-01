c1cb2ea8b3798e1c786190ec6abbe510ab781736
ValerieMalicka <vqm5328@psu.edu>
2023-08-31 20:19:09 -0400

    Dog project

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
-        Dog dog1 = new Dog(args[0], age: 5, color: "Yellow");
+        Dog dog1 = new Dog(args[0], 5,"yellow");
+        System.out.println("Dog Name's: " + dog1.getName());
+
     }
 }
