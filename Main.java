6748511289f1ba8cf60f1b6d92ecd35000431f1d
ValerieMalicka <vqm5328@psu.edu>
2023-09-01 09:24:30 -0400

    Dog project

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Chloe",5,"Yellow", 10, "German Shepard");
+        Dog myDog = new Dog("Chloe", 5, "Yellow", 70, "German Shepard");
         myDog.bark();
         myDog.eat();
         myDog.sleep();
@@ -11,6 +11,32 @@ public class Main {
         System.out.println("age: " + myDog.getAge());
         System.out.println("color: " + myDog.getColor());
         System.out.println("breed: " + myDog.getBreed());
+
+
+        Dog myDog2 = new Dog("Scoobie",3,"Orange", 10, "Lab Mix");
+        myDog2.bark();
+        myDog2.eat();
+        myDog2.sleep();
+        myDog2.walk();
+        myDog2.sit();
+
+        System.out.println("name: " + myDog2.getName());
+        System.out.println("age: " + myDog2.getAge());
+        System.out.println("color: " + myDog2.getColor());
+        System.out.println("breed: " + myDog2.getBreed());
+
+        Dog myDog3 = new Dog("Hidie",7,"Black and White", 30, "Huskey");
+        myDog3.bark();
+        myDog3.eat();
+        myDog3.sleep();
+        myDog3.walk();
+        myDog3.sit();
+
+        System.out.println("name: " + myDog3.getName());
+        System.out.println("age: " + myDog3.getAge());
+        System.out.println("color: " + myDog3.getColor());
+        System.out.println("breed: " + myDog3.getBreed());
+
     }
 
 }
